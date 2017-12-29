e64d377b8799ef74e38b6d0dc3a948ee2125ca43
EXODIALOSTKNIGHT <EXODIALOSTKNIGHT@GMAIL.COM>
2017-12-30 02:12:08 +0800

    tata

--- Build/Android/src/ring/K/OBBData.java
+++ Build/Android/src/ring/K/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-15471473L // the length of the file in bytes
+15480375L // the length of the file in bytes
 )
 };
 };
